32a86ca254a19c89fbf4344318687ddfc7617beb
arc9693 <archana.choudhary.9693@gmail.com>
2019-01-30 01:22:49 +0530

    Update urls

--- IntReview/urls.py
+++ IntReview/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('Interviewbook/', include('Interviewbook.urls')),
+    path('', include('Interviewbook.urls')),
     path('admin/', admin.site.urls),
 ]
