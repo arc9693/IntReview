9da4e575cb50e048a3098af78dfcef365fbbcafd
Chirag Agarwal <chiragagarwal54@yahoo.com>
2019-02-02 18:09:53 +0530

    Corrected Delete response button not working issue

--- Interviewbook/views.py
+++ Interviewbook/views.py
@@ -82,6 +82,7 @@ def updateResponse(request, response_id):
 
 @login_required(login_url='login')
 def deleteResponse(request,response_id):
+    instance = get_object_or_404(InterviewResponse, id=response_id)
     if instance.name.pk==request.user.pk:
         instance = get_object_or_404(InterviewResponse, id=response_id).delete()
     return redirect('ListResponses')
